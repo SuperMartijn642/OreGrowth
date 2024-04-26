a8ac51009625d4157f7719e33308c031bf58e1ba
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-04-26 21:09:22 +0200

    Cache whether a block has an ore growth recipe in `BlockStateBaseMixin` #21

--- src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,15 +21,26 @@ import java.util.Random;
 @Mixin(BlockBehaviour.BlockStateBase.class)
 public class BlockStateBaseMixin {
 
+    @Unique
+    private boolean isRandomlyTicking;
+
+    @Inject(
+        method = "initCache",
+        at = @At("TAIL")
+    )
+    private void initCache(CallbackInfo ci){
+        //noinspection DataFlowIssue
+        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
+        this.isRandomlyTicking = OreGrowthRecipeManager.getRecipeFor(state.getBlock()) != null;
+    }
+
     @Inject(
         method = "isRandomlyTicking",
         at = @At("HEAD"),
         cancellable = true
     )
-    private void initCache(CallbackInfoReturnable<Boolean> ci){
-        //noinspection DataFlowIssue
-        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
-        if(OreGrowthRecipeManager.getRecipeFor(state.getBlock()) != null)
+    private void isRandomlyTicking(CallbackInfoReturnable<Boolean> ci){
+        if(this.isRandomlyTicking)
             ci.setReturnValue(true);
     }
 
@@ -37,6 +49,8 @@ public class BlockStateBaseMixin {
         at = @At("HEAD")
     )
     private void randomTick(ServerLevel level, BlockPos pos, Random random, CallbackInfo ci){
+        if(!this.isRandomlyTicking)
+            return;
         //noinspection DataFlowIssue
         BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
         OreGrowthRecipe recipe = OreGrowthRecipeManager.getRecipeFor(state.getBlock());
