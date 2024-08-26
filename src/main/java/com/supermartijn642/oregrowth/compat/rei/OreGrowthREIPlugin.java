1fc3a7499a49a0fbab2cbcffc7859f2f1916f597
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-27 01:31:21 +0200

    Add Roughly Enough Items support

---
+++ src/main/java/com/supermartijn642/oregrowth/compat/rei/OreGrowthREIPlugin.java
@@ -0,0 +1,34 @@
+package com.supermartijn642.oregrowth.compat.rei;
+
+import com.supermartijn642.oregrowth.OreGrowth;
+import com.supermartijn642.oregrowth.content.OreGrowthRecipeManager;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
+
+/**
+ * Created 26/08/2024 by SuperMartijn642
+ */
+public class OreGrowthREIPlugin implements REIClientPlugin {
+
+    public static final CategoryIdentifier<OreGrowthREIDisplay> ORE_GROWTH_CATEGORY = CategoryIdentifier.of(OreGrowth.MODID, "ore_growth");
+
+    @Override
+    public void registerCategories(CategoryRegistry registry){
+        registry.add(new OreGrowthREIRecipeCategory());
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry){
+        OreGrowthRecipeManager.getAllRecipes().stream()
+            .map(OreGrowthREIDisplay::new)
+            .forEach(registry::add);
+    }
+
+    @Override
+    public void registerDisplaySerializer(DisplaySerializerRegistry registry){
+        REIClientPlugin.super.registerDisplaySerializer(registry); // TODO
+    }
+}
