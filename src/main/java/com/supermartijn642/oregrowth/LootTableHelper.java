907225c5c546a59c6b25d6b70503cde86a952d8b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-12-21 14:57:11 +0100

    Filter null formatted loot table conditions

--- src/main/java/com/supermartijn642/oregrowth/LootTableHelper.java
+++ src/main/java/com/supermartijn642/oregrowth/LootTableHelper.java
@@ -111,7 +111,7 @@ public class LootTableHelper {
     }
 
     private static Stream<LootEntry> itemsFromContainer(LootPoolEntryContainer container, int totalWeight, Function<ResourceLocation,LootTable> lookup){
-        List<LootEntryConditions> conditions = Arrays.stream(container.conditions).map(LootTableHelper::formatCondition).toList();
+        List<LootEntryConditions> conditions = Arrays.stream(container.conditions).map(LootTableHelper::formatCondition).filter(Objects::nonNull).toList();
         if(container instanceof AlternativesEntry){
             MutableComponent not = TextComponents.translation("oregrowth.jei_category.conditions.none_of").get();
             Collection<LootEntryConditions> previousConditions = new LinkedHashSet<>();
@@ -121,7 +121,7 @@ public class LootTableHelper {
                 if(!previousConditions.isEmpty())
                     stream = stream.map(entry -> entry.prependConditions(Stream.concat(conditions.stream(), Stream.of(new LootEntryConditions(not, new ArrayList<>(previousConditions)))), conditions.size() + 1));
                 stream.forEach(entries::add);
-                Arrays.stream(child.conditions).map(LootTableHelper::formatCondition).forEach(previousConditions::add);
+                Arrays.stream(child.conditions).map(LootTableHelper::formatCondition).filter(Objects::nonNull).forEach(previousConditions::add);
             }
             return entries.stream();
         }
