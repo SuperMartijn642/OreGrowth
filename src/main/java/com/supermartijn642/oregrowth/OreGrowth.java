080c6af45619fe646541b4c8a781878ecad857f0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-12-27 18:32:46 +0100

    Update to 1.21.4

--- src/main/java/com/supermartijn642/oregrowth/OreGrowth.java
+++ src/main/java/com/supermartijn642/oregrowth/OreGrowth.java
@@ -46,8 +46,8 @@ public class OreGrowth {
     public static void registerGenerators(){
         GeneratorRegistrationHandler handler = GeneratorRegistrationHandler.get(MODID);
         handler.addGenerator(OreGrowthBlockStateGenerator::new);
+        handler.addGenerator(OreGrowthItemInfoGenerator::new);
         handler.addGenerator(OreGrowthLanguageGenerator::new);
-        handler.addGenerator(OreGrowthModelGenerator::new);
         handler.addGenerator(OreGrowthOreGrowthRecipeGenerator::new);
         handler.addGenerator(OreGrowthTagGenerator::new);
     }
