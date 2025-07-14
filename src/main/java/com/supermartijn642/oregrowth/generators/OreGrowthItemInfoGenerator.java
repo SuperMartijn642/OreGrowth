c08e39d13d5fb01440428e27a8a9317075fc514f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-14 16:54:58 +0200

    Add separate block for fully grown crystals

--- src/main/java/com/supermartijn642/oregrowth/generators/OreGrowthItemInfoGenerator.java
+++ src/main/java/com/supermartijn642/oregrowth/generators/OreGrowthItemInfoGenerator.java
@@ -16,5 +16,6 @@ public class OreGrowthItemInfoGenerator extends ItemInfoGenerator {
     @Override
     public void generate(){
         this.simpleInfo(OreGrowth.ORE_GROWTH_ITEM, "block/ore_growth_stage_4");
+        this.simpleInfo(OreGrowth.COMPLETE_ORE_GROWTH_ITEM, "block/ore_growth_stage_4");
     }
 }
