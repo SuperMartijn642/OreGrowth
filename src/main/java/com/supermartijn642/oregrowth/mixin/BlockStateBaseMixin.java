e4a022b4a872299311647478cdae26b29a1e07f5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-14 16:25:13 +0200

    Store harvest tool and harvest tier as block state properties

--- src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
@@ -1,16 +1,20 @@
 package com.supermartijn642.oregrowth.mixin;
 
+import com.supermartijn642.oregrowth.OreGrowth;
 import com.supermartijn642.oregrowth.content.OreGrowthBlock;
 import com.supermartijn642.oregrowth.content.OreGrowthRecipe;
 import com.supermartijn642.oregrowth.content.OreGrowthRecipeManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Created 04/10/2023 by SuperMartijn642
@@ -38,6 +42,34 @@ public class BlockStateBaseMixin {
         BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
         OreGrowthRecipe recipe = OreGrowthRecipeManager.get(level.isClientSide).getRecipeFor(state.getBlock());
         if(recipe != null)
-            OreGrowthBlock.trySpawnOreGrowth(recipe, level, pos, random);
+            OreGrowthBlock.trySpawnOreGrowth(state, recipe, level, pos, random);
+    }
+
+    @Inject(
+        method = "is(Lnet/minecraft/tags/TagKey;)Z",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void is(TagKey<Block> tag, CallbackInfoReturnable<Boolean> ci){
+        // Intercept checks for mining tags
+        //noinspection DataFlowIssue
+        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
+        Block block = state.getBlock();
+        if((block == OreGrowth.ORE_GROWTH_BLOCK || block == OreGrowth.COMPLETE_ORE_GROWTH_BLOCK)
+            && ((OreGrowthBlock)block).is(state, tag))
+            ci.setReturnValue(true);
+    }
+
+    @Inject(
+        method = "requiresCorrectToolForDrops()Z",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void requiresCorrectToolForDrops(CallbackInfoReturnable<Boolean> ci){
+        //noinspection DataFlowIssue
+        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
+        Block block = state.getBlock();
+        if(block == OreGrowth.ORE_GROWTH_BLOCK || block == OreGrowth.COMPLETE_ORE_GROWTH_BLOCK)
+            ci.setReturnValue(((OreGrowthBlock)block).requiresCorrectToolForDrops(state));
     }
 }
