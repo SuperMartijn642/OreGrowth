a8083b9b5aae1d3150b8a4d45c663adbc2e1e64d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-21 15:06:48 +0200

    Overwrite `canHarvestBlock` for ore growth block

---
+++ src/main/java/com/supermartijn642/oregrowth/mixin/ServerPlayerGameModeMixin.java
@@ -0,0 +1,45 @@
+package com.supermartijn642.oregrowth.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.supermartijn642.oregrowth.OreGrowth;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+/**
+ * Created 21/07/2025 by SuperMartijn642
+ */
+@Mixin(ServerPlayerGameMode.class)
+public class ServerPlayerGameModeMixin {
+
+    @Shadow
+    private ServerLevel level;
+    @Final
+    @Shadow
+    protected ServerPlayer player;
+
+    @ModifyExpressionValue(
+        method = "destroyBlock",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/level/ServerPlayer;hasCorrectToolForDrops(Lnet/minecraft/world/level/block/state/BlockState;)Z"
+        )
+    )
+    private boolean correctToolCheck(boolean original, BlockPos pos){
+        BlockState state = this.level.getBlockState(pos);
+        Block block = state.getBlock();
+        if(block == OreGrowth.ORE_GROWTH_BLOCK || block == OreGrowth.COMPLETE_ORE_GROWTH_BLOCK){
+            pos = pos.relative(state.getValue(BlockStateProperties.FACING));
+            return this.player.hasCorrectToolForDrops(this.level.getBlockState(pos));
+        }
+        return original;
+    }
+}
