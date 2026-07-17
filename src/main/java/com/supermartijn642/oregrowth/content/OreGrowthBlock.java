144c4ac59e7f1581e3ef5768ab509eaa58d53dda
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-17 17:14:30 +0200

    Cancel drops from pistons when base block requires a tool

--- src/main/java/com/supermartijn642/oregrowth/content/OreGrowthBlock.java
+++ src/main/java/com/supermartijn642/oregrowth/content/OreGrowthBlock.java
@@ -32,6 +32,7 @@ import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
@@ -269,6 +270,11 @@ public class OreGrowthBlock extends BaseBlock implements SimpleWaterloggedBlock 
 
     public boolean is(BlockStateBase state, TagKey<Block> tag){
         return tag.equals(state.getValue(HARVEST_TOOL).tag) || tag.equals(state.getValue(TOOL_TIER).tag);
+    }
+
+    @Override
+    public @Nullable PushReaction getPistonPushReaction(BlockState state){
+        return PushReaction.DESTROY;
     }
 
     public enum HarvestTool implements StringRepresentable {
