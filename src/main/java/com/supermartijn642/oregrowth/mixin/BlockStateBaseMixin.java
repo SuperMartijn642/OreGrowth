33e16956f69b19aa08b9b0f7601717457640d67f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-21 00:52:03 +0200

    Add random offset to crystals

--- src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
@@ -8,8 +8,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderSet;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -101,5 +103,18 @@ public class BlockStateBaseMixin {
         Block block = state.getBlock();
         if(OreGrowth.isOreGrowthBlock(block))
             ci.setReturnValue(((OreGrowthBlock)block).requiresCorrectToolForDrops(state));
+    }
+
+    @Inject(
+        method = "getOffset",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void getOffset(BlockGetter level, BlockPos pos, CallbackInfoReturnable<Vec3> ci) {
+        //noinspection DataFlowIssue
+        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
+        Block block = state.getBlock();
+        if(OreGrowth.isOreGrowthBlock(block))
+            ci.setReturnValue(((OreGrowthBlock)block).getOffset(level, pos, state));
     }
 }
