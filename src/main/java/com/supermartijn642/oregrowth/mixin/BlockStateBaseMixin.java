c61a6e43a33cce885fb44b9319c6b020dec1a4fc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-21 15:53:27 +0200

    Add `ModList` null check in `BlockStateBaseMixin` because NeoForge already loads parts of the game before initializing mods

--- src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/supermartijn642/oregrowth/mixin/BlockStateBaseMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -83,6 +84,8 @@ public abstract class BlockStateBaseMixin implements OreGrowthBlockState {
         cancellable = true
     )
     private void getOffset(BlockPos pos, CallbackInfoReturnable<Vec3> ci) {
+        if(ModList.get() == null) // This is needed because NeoForge already loads blocks before mods are initialized
+            return;
         //noinspection DataFlowIssue
         BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase)(Object)this;
         Block block = state.getBlock();
